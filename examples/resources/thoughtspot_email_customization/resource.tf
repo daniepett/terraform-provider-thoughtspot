94228f65d3dfa02796a3eb7ba0cbbcf16576e0b1
Daniel Pettersen <dpettersen@dunelm.com>
2025-09-15 15:59:24 +0100

    Add docs

---
+++ examples/resources/thoughtspot_email_customization/resource.tf
@@ -0,0 +1,8 @@
+resource "thoughtspot_email_customization" "this" {
+  cta_button_bg_color    = "#0b8a00"
+  cta_text_font_color    = "#ffffff"
+  primary_bg_color       = "#f8f8f8"
+  hide_footer_phone      = true
+  hide_mobile_app_nudge  = true
+  validate_customization = true
+}
